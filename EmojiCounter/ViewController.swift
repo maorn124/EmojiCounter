f0995666a06b715c501b88b9c95bad4ef45027c6
maorn124 <maor.niv@gmail.com>
2024-05-16 19:45:46 +0300

    Initial Commit

---
+++ EmojiCounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiCounter
+//
+//  Created by Maor Niv on 5/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
